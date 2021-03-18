00a59db56d121da2ecc1b7448f012b272629b642
u235 <sanchez_ya_llego@hotmail.com>
2021-03-18 23:42:28 +0100

    Se agrega método de extensión ListExtensions.Replace<T>

---
+++ Extensions/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Venn.Extensions
+{
+    public static class ListExtensions
+    {
+        public static IList<T> Replace<T>(this IList<T> aList, IList<T> bList, Func<T,T,bool> comparer)
+        {
+            aList.ToList().ForEach(itemA =>
+            {
+                bList.ToList().ForEach(itemB => 
+                {
+                    if(comparer(itemA, itemB))
+                    {
+                        aList[aList.IndexOf(itemA)] = itemB;
+                    }
+                });
+            });
+
+            return aList;
+        }
+    }
+
+}
