3172df7a20719133e0ff7a61df3253bf335cc681
u235 <sanchez_ya_llego@hotmail.com>
2023-12-21 08:27:16 +0100

    SSA: NotifyPropertyChangedWrapper, VennComparer

---
+++ Comparers/VennComparer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Venn.Comparers
+{
+    /// <summary>
+    /// Custom comparer class implementing IEqualityComparer for Venn diagrams.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to compare.</typeparam>
+    public class VennComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _comparerExp;
+
+        /// <summary>
+        /// Initializes a new instance of the VennComparer class with the specified comparison expression.
+        /// </summary>
+        /// <param name="comparerExp">The comparison expression defining the equality between objects of type T.</param>
+        /// <exception cref="ArgumentNullException">Thrown when comparerExp is null.</exception>
+        public VennComparer(Func<T, T, bool> comparerExp)
+        {
+            if (comparerExp == null)
+                throw new ArgumentNullException(nameof(comparerExp));
+
+            _comparerExp = comparerExp;
+        }
+
+        /// <summary>
+        /// Determines whether two objects are equal based on the comparison expression.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>True if the objects are equal according to the comparison expression; otherwise, false.</returns>
+        public bool Equals(T x, T y)
+        {
+            return _comparerExp(x, y);
+        }
+
+        /// <summary>
+        /// Returns a hash code for the specified object.
+        /// </summary>
+        /// <param name="obj">The object for which to get a hash code.</param>
+        /// <returns>A hash code for the specified object.</returns>
+        public int GetHashCode(T obj)
+        {
+            return obj.ToString().ToLower().GetHashCode();
+        }
+    }
+}
+
--- PropertyChanges/NotifyPropertyChangedWrapper.cs
+++ PropertyChanges/NotifyPropertyChangedWrapper.cs
@@ -8,20 +8,37 @@ using System.Text;
 
 namespace Venn.PropertyChanges
 {
+    /// <summary>
+    /// Class that wraps an object not implementing INotifyPropertyChanged and notifies property changes.
+    /// </summary>
+    /// <typeparam name="T">Type of the object to wrap.</typeparam>
     public class NotifyPropertyChangedWrapper<T> : INotifyPropertyChanged
     {
         private readonly T _wrappedObject;
         private readonly BehaviorSubject<Unit> _propertyChangedSubject = new BehaviorSubject<Unit>(Unit.Default);
 
+        /// <summary>
+        /// Initializes a new instance of the NotifyPropertyChangedWrapper class.
+        /// </summary>
+        /// <param name="wrappedObject">Object to wrap.</param>
         public NotifyPropertyChangedWrapper(T wrappedObject)
         {
             _wrappedObject = wrappedObject ?? throw new ArgumentNullException(nameof(wrappedObject));
         }
 
+        /// <summary>
+        /// Gets an observable that emits events whenever a property of the wrapped object changes.
+        /// </summary>
         public IObservable<Unit> PropertyChangedObservable => _propertyChangedSubject.AsObservable();
 
+        /// <summary>
+        /// Gets the wrapped object.
+        /// </summary>
         public T WrappedObject => _wrappedObject;
 
+        /// <summary>
+        /// Event that is triggered when a property of the wrapped object changes.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged(string propertyName)
@@ -30,10 +47,14 @@ namespace Venn.PropertyChanges
             _propertyChangedSubject.OnNext(Unit.Default);
         }
 
+        /// <summary>
+        /// Static method to wrap an object and create an instance of NotifyPropertyChangedWrapper.
+        /// </summary>
+        /// <param name="obj">Object to wrap.</param>
+        /// <returns>Instance of NotifyPropertyChangedWrapper wrapping the provided object.</returns>
         public static NotifyPropertyChangedWrapper<T> Wrap(T obj)
         {
             return new NotifyPropertyChangedWrapper<T>(obj);
         }
     }
-
 }
