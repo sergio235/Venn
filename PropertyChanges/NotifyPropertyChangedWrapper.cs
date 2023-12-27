ff078ae033dd1215d04928eb72e7282930f93448
u235 <sanchez_ya_llego@hotmail.com>
2023-12-27 19:04:25 +0100

    SSA: Documentación de NotifyPropertyChangedWrapper

--- PropertyChanges/NotifyPropertyChangedWrapper.cs
+++ PropertyChanges/NotifyPropertyChangedWrapper.cs
@@ -4,19 +4,33 @@ using System.Reactive.Subjects;
 
 namespace Venn.PropertyChanges
 {
+    /// <summary>
+    /// Wraps an object that does not implement INotifyPropertyChanged and notifies about property changes.
+    /// </summary>
+    /// <typeparam name="T">Type of the object to wrap.</typeparam>
     public class NotifyPropertyChangedWrapper<T> : INotifyPropertyChanged
     {
         private T _value;
         private readonly BehaviorSubject<T> _valueChanged;
 
+        /// <summary>
+        /// Initializes a new instance of the NotifyPropertyChangedWrapper class.
+        /// </summary>
+        /// <param name="initialValue">Initial value for the wrapped object.</param>
         public NotifyPropertyChangedWrapper(T initialValue)
         {
             _value = initialValue;
             _valueChanged = new BehaviorSubject<T>(_value);
         }
 
+        /// <summary>
+        /// Gets an observable that emits events whenever the property of the wrapped object changes.
+        /// </summary>
         internal IObservable<T> Observable => _valueChanged.AsObservable();
 
+        /// <summary>
+        /// Gets or sets the wrapped object's value.
+        /// </summary>
         public T Value
         {
             get => _value;
@@ -33,6 +47,9 @@ namespace Venn.PropertyChanges
             }
         }
 
+        /// <summary>
+        /// Event that is triggered when a property of the wrapped object changes.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged(string propertyName)
