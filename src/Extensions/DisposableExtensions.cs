5e770ccb5415a0b63b03c55f36b050547b64e974
u235 <sanchez_ya_llego@hotmail.com>
2023-12-28 19:51:09 +0100

    SSA: v1.0.2: Se actualiza README

---
+++ src/Extensions/DisposableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venn.Extensions
+{
+    public static class DisposableExtensions
+    {
+        public static IDisposable DisposeWith(this IDisposable disposable, CompositeDisposable compositeDisposable)
+        {
+            if (disposable == null)
+            {
+                throw new ArgumentNullException(nameof(disposable));
+            }
+
+            if (compositeDisposable == null)
+            {
+                throw new ArgumentNullException(nameof(compositeDisposable));
+            }
+
+            compositeDisposable.Add(disposable);
+
+            return disposable;
+        }
+    }
+}
