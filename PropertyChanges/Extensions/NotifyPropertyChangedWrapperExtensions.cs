453791d5b3824f2818cb428c7999723dae2a7730
u235 <sanchez_ya_llego@hotmail.com>
2023-12-28 01:18:56 +0100

    SSA: NotifyPropertyChangedWrapperExtensions

---
+++ PropertyChanges/Extensions/NotifyPropertyChangedWrapperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Venn.PropertyChanges
+{
+    public static class NotifyPropertyChangedWrapperExtensions
+    {
+        public static NotifyPropertyChangedWrapper<T> Wrap<T>(this T obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            return new NotifyPropertyChangedWrapper<T>(obj);
+        }
+    }
+}
