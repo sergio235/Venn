034a6d46e7ee5d2aa8feebd3a9ccd6f60a31596f
u235 <sanchez_ya_llego@hotmail.com>
2024-05-20 17:21:22 +0200

    u235: Se agregan métodos asíncronos a INotifyPropertyChangedExtensions y se agrega en ObservableCollectionExtensions método que acepta IScheduler.

--- Venn/Extensions/INotifyPropertyChangedExtensions.cs
+++ Venn/Extensions/INotifyPropertyChangedExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Linq.Expressions;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using Venn.Base.Interfaces;
 using Venn.PropertyChanges;
@@ -27,6 +28,24 @@ namespace Venn.Extensions
             return CreateObservable(source, propertySelector);
         }
 
+        #endregion
+
+        #region WhenAnyAsync
+        public static async Task<IObservable<TProperty>> WhenAnyAsync<T, TProperty>(
+            this T source,
+            Expression<Func<T, TProperty>> propertySelector,
+            IScheduler observeOnScheduler = null)
+            where T : INotifyPropertyChanged
+        {
+            // Check for null parameters
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (propertySelector == null)
+                throw new ArgumentNullException(nameof(propertySelector));
+
+            return await CreateObservableAsync(source, propertySelector, observeOnScheduler);
+        }
         #endregion
 
         #region WhenAnyValue
@@ -58,12 +77,34 @@ namespace Venn.Extensions
 
         #endregion
 
+        #region WhenAnyValueAsync
+        public static async Task<IObservable<TProperty>> WhenAnyValueAsync<T, TProperty>(
+            this T source,
+            Expression<Func<T, TProperty>> propertySelector,
+            IScheduler observeOnScheduler = null)
+            where T : INotifyPropertyChanged
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (propertySelector == null)
+                throw new ArgumentNullException(nameof(propertySelector));
+
+            var observable = CreateObservable(source, propertySelector, observeOnScheduler);
+            return await Task.FromResult(observable);
+        }
+        #endregion
+
         // Helper method to create the observable for property changes
         private static IObservable<TProperty> CreateObservable<T, TProperty>(
             T source,
             Expression<Func<T, TProperty>> propertySelector)
             where T : INotifyPropertyChanged
         {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (propertySelector == null)
+                throw new ArgumentNullException(nameof(propertySelector));
+
             var propertyName = propertySelector.GetPropertyName();
             var isSuspendable = source is ISuspendableNotifyPropertyChanged;
             var suspendable = isSuspendable ? source as ISuspendableNotifyPropertyChanged : null;
@@ -76,5 +117,30 @@ namespace Venn.Extensions
                 .Select(_ => propertySelector.Compile()(source));
         }
 
+        private static IObservable<TProperty> CreateObservable<T, TProperty>(
+            T source,
+            Expression<Func<T, TProperty>> propertySelector,
+            IScheduler observeOnScheduler = null)
+            where T : INotifyPropertyChanged
+        {
+            var scheduler = observeOnScheduler ?? CurrentThreadScheduler.Instance;
+
+            var observable = CreateObservable(source, propertySelector)
+                .StartWith(propertySelector.Compile()(source))
+                .ObserveOn(scheduler); // Observa los cambios en el hilo especificado o el actual
+
+            return observable;
+        }
+
+        public static Task<IObservable<TProperty>> CreateObservableAsync<T, TProperty>(
+            T source,
+            Expression<Func<T, TProperty>> propertySelector,
+            IScheduler observeOnScheduler = null)
+            where T : INotifyPropertyChanged
+        {
+            var observable = CreateObservable(source, propertySelector, observeOnScheduler);
+
+            return Task.FromResult(observable);
+        }
     }
 }
--- Venn/Extensions/ObservableCollectionExtensions.cs
+++ Venn/Extensions/ObservableCollectionExtensions.cs
@@ -1,102 +1,98 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq.Expressions;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using Venn.Extensions;
 
-namespace Venn.Extensions
+public static class ObservableCollectionExtensions
 {
-    public static class ObservableCollectionExtensions
+    public static void WhenAnyItem<T>(
+        this ObservableCollection<T> collection,
+        Action<T> onPropertyChange,
+        NotifyCollectionChangedAction actionsToObserve = NotifyCollectionChangedAction.Add | NotifyCollectionChangedAction.Remove | NotifyCollectionChangedAction.Replace | NotifyCollectionChangedAction.Reset,
+        params Expression<Func<T, object>>[] propertySelectors)
+        where T : INotifyPropertyChanged
     {
-        public static void ObservePropertyChanges<T>(
-            this ObservableCollection<T> collection,
-            Action<T> onPropertyChange,
-            NotifyCollectionChangedAction actionsToObserve = NotifyCollectionChangedAction.Add | NotifyCollectionChangedAction.Remove | NotifyCollectionChangedAction.Replace | NotifyCollectionChangedAction.Reset,
-            params Expression<Func<T, object>>[] propertySelectors)
-            where T : INotifyPropertyChanged
+        WhenAnyItem(collection, onPropertyChange, null, actionsToObserve, propertySelectors);
+    }
+
+    public static void WhenAnyItem<T>(
+        this ObservableCollection<T> collection,
+        Action<T> onPropertyChange,
+        IScheduler observeOnScheduler,
+        NotifyCollectionChangedAction actionsToObserve = NotifyCollectionChangedAction.Add | NotifyCollectionChangedAction.Remove | NotifyCollectionChangedAction.Replace | NotifyCollectionChangedAction.Reset,
+        params Expression<Func<T, object>>[] propertySelectors)
+        where T : INotifyPropertyChanged
+    {
+        collection.CollectionChanged += (sender, e) =>
         {
-            collection.CollectionChanged += (sender, e) =>
+            if ((actionsToObserve & e.Action) != 0)
             {
-                if ((actionsToObserve & e.Action) != 0)
+                if (e.Action == NotifyCollectionChangedAction.Add || e.Action == NotifyCollectionChangedAction.Replace)
                 {
-                    if (e.Action == NotifyCollectionChangedAction.Add || e.Action == NotifyCollectionChangedAction.Replace)
+                    foreach (T item in e.NewItems)
                     {
-                        foreach (T item in e.NewItems)
+                        AttachPropertyChangedHandlers(item, propertySelectors, onPropertyChange, observeOnScheduler);
+                    }
+                }
+                else if (e.Action == NotifyCollectionChangedAction.Remove || e.Action == NotifyCollectionChangedAction.Replace)
+                {
+                    foreach (T item in e.OldItems)
+                    {
+                        DetachPropertyChangedHandlers(item, propertySelectors, onPropertyChange);
+                    }
+                }
+                else if (e.Action == NotifyCollectionChangedAction.Reset)
+                {
+                    if (actionsToObserve.HasFlag(NotifyCollectionChangedAction.Reset))
+                    {
+                        foreach (T item in collection)
                         {
-                            AttachPropertyChangedHandlers(item, propertySelectors, onPropertyChange);
+                            AttachPropertyChangedHandlers(item, propertySelectors, onPropertyChange, observeOnScheduler);
                         }
                     }
-                    else if (e.Action == NotifyCollectionChangedAction.Remove || e.Action == NotifyCollectionChangedAction.Replace)
+                }
+            }
+        };
+    }
+
+    private static void AttachPropertyChangedHandlers<T>(T item, Expression<Func<T, object>>[] propertySelectors, Action<T> onPropertyChange, IScheduler observeOnScheduler) where T : INotifyPropertyChanged
+    {
+        foreach (var propertySelector in propertySelectors)
+        {
+            var propertyName = propertySelector.GetPropertyName();
+            item.PropertyChanged += (s, args) =>
+            {
+                if (args.PropertyName == propertyName)
+                {
+                    if (observeOnScheduler != null)
                     {
-                        foreach (T item in e.OldItems)
-                        {
-                            DetachPropertyChangedHandlers(item, propertySelectors, onPropertyChange);
-                        }
+                        observeOnScheduler.Schedule(() => onPropertyChange(item));
                     }
-                    else if (e.Action == NotifyCollectionChangedAction.Reset)
+                    else
                     {
-                        if (actionsToObserve.HasFlag(NotifyCollectionChangedAction.Reset))
-                        {
-                            foreach (T item in collection)
-                            {
-                                AttachPropertyChangedHandlers(item, propertySelectors, onPropertyChange);
-                            }
-                        }
+                        onPropertyChange(item);
                     }
                 }
             };
         }
+    }
 
-        public static int FindIndex<T>(this ObservableCollection<T> ts, Predicate<T> match)
+    private static void DetachPropertyChangedHandlers<T>(T item, Expression<Func<T, object>>[] propertySelectors, Action<T> onPropertyChange) where T : INotifyPropertyChanged
+    {
+        foreach (var propertySelector in propertySelectors)
         {
-            return ts.FindIndex(0, ts.Count, match);
-        }
-
-        public static int FindIndex<T>(this ObservableCollection<T> ts, int startIndex, Predicate<T> match)
-        {
-            return ts.FindIndex(startIndex, ts.Count, match);
-        }
-
-        public static int FindIndex<T>(this ObservableCollection<T> ts, int startIndex, int count, Predicate<T> match)
-        {
-            if (startIndex < 0) startIndex = 0;
-            if (count > ts.Count) count = ts.Count;
-
-            for (int i = startIndex; i < count; i++)
+            var propertyName = propertySelector.GetPropertyName();
+            item.PropertyChanged -= (s, args) =>
             {
-                if (match(ts[i])) return i;
-            }
-
-            return -1;
-        }
-
-        private static void AttachPropertyChangedHandlers<T>(T item, Expression<Func<T, object>>[] propertySelectors, Action<T> onPropertyChange) where T : INotifyPropertyChanged
-        {
-            foreach (var propertySelector in propertySelectors)
-            {
-                var propertyName = propertySelector.GetPropertyName();
-                item.PropertyChanged += (s, args) =>
+                if (args.PropertyName == propertyName)
                 {
-                    if (args.PropertyName == propertyName)
-                    {
-                        onPropertyChange(item);
-                    }
-                };
-            }
-        }
-
-        private static void DetachPropertyChangedHandlers<T>(T item, Expression<Func<T, object>>[] propertySelectors, Action<T> onPropertyChange) where T : INotifyPropertyChanged
-        {
-            foreach (var propertySelector in propertySelectors)
-            {
-                var propertyName = propertySelector.GetPropertyName();
-                item.PropertyChanged -= (s, args) =>
-                {
-                    if (args.PropertyName == propertyName)
-                    {
-                        onPropertyChange(item);
-                    }
-                };
-            }
+                    onPropertyChange(item);
+                }
+            };
         }
     }
 }
